089d81c428f5f408abab5ea639b4fedaebfc0f2a
beagledeacero97 <ivanjuarezrin97@gmail.com>
2022-03-04 11:47:47 +0100

    Control excepciones

--- src/control/Menu.java
+++ src/control/Menu.java
@@ -10,10 +10,24 @@ public class Menu {
 	
 	public void menu() {
 		System.out.println("Bienvenido a LucaSteam Game Software.");
-		System.out.println("Opciones disponibles: 1-Dar de alta un juego. 2-Dar de baja un juego. 3-Modificar un juego. 4-Generar informes");
-		int seleccion = PedirDatos.pedirEnteros("Introduce el numero de la opcion requerida y pulsa enter.");
-	
+		boolean salir = false;
 		
+		while(salir==false) {
+			System.out.println("Opciones disponibles: 1-Dar de alta un juego. 2-Dar de baja un juego. 3-Modificar un juego. 4-Generar informes. 5-Salir");
+			
+			try {
+				int seleccion = PedirDatos.pedirEnteros("Introduce el numero de la opcion requerida y pulsa enter.");
+				
+				switch(seleccion) {
+				case 1: 
+				}
+			}catch(NumberFormatException e) {
+				System.out.println("Introduce una opcion valida.");
+			}
+			
+		}
 	}
+	
+	
 	
 }
