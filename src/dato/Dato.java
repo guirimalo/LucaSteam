0ceb02fb1162ed2fd2b663bd46705b7039416c12
Samuel <samuelgutierrez147@gmail.com>
2022-03-04 13:05:44 +0100

    Se ha creado el método altaJuego en la clase Gestor y en Dato

--- src/dato/Dato.java
+++ src/dato/Dato.java
@@ -1,5 +1,8 @@
 package dato;
 import java.io.*;
+import model.Juego;
+import servicio.Gestor;
+
 public class Dato {
     public static void main(String[] args) {
         recogerDatos();
@@ -7,6 +10,8 @@ public class Dato {
     static String file = "src\\vgsales.csv";
     static BufferedReader reader = null;
     static String line = "";
+    
+    
     public static void recogerDatos(){
         try {
             reader = new BufferedReader(new FileReader (file));
@@ -23,5 +28,21 @@ public class Dato {
     }
     public void divisorDatos(){
 
+    }
+    
+    public Juego altaJuego(Juego juego) {
+    	/*if(revisarJuego(juego)) {
+    	
+    	}*/
+    	g.altaJuego(juego);
+		return juego;
+    	
+    }
+    
+    //Pendiente de completar
+    
+    public boolean revisarJuego(Juego juego) {
+		return true;
+    	
     }
 }
