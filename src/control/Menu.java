020a8e055129ea6c2f36fa6ec3f64728bd1ad88c
beagledeacero97 <ivanjuarezrin97@gmail.com>
2022-03-04 10:47:09 +0100

    Menu creado

--- src/control/Menu.java
+++ src/control/Menu.java
@@ -1,4 +1,19 @@
 package control;
 
+import utilidades.PedirDatos;
+
 public class Menu {
+	
+	public void iniciar() {
+		this.menu();
+	}
+	
+	public void menu() {
+		System.out.println("Bienvenido a LucaSteam Game Software.");
+		System.out.println("Opciones disponibles: 1-Dar de alta un juego. 2-Dar de baja un juego. 3-Modificar un juego. 4-Generar informes");
+		int seleccion = PedirDatos.pedirEnteros("Introduce el numero de la opcion requerida y pulsa enter.");
+	
+		
+	}
+	
 }
