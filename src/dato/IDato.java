9fc09331cf4dcc317434f14555b46244f1962e49
miguelbayoDAW <miguelpuerta16@gmail.com>
2022-03-08 15:18:44 +0100

    Modificación métodos interfaces

--- src/dato/IDato.java
+++ src/dato/IDato.java
@@ -1,11 +1,25 @@
 package dato;
 
+import java.util.ArrayList;
+
 import model.Juego;
 
 public interface IDato {
 	void recogerDatos();
-
+	boolean isExiste(Juego juego);
 	void generarInforme();
-
-	Juego altaJuego();
+	void pedirGenero();
+	ArrayList<Juego> filtradoGenero(String genero);
+	ArrayList<Juego> filtradoSXX();
+	void pedirPlataforma();
+	ArrayList<Juego> filtradoPlataforma(String p);
+	ArrayList<Juego> filtradoAniosPares();
+	boolean isPlataforma(String plataforma);
+	boolean isGenero(String genero);
+	ArrayList<Juego> filtradoPorNintendo();
+	ArrayList<Juego> filtrarPorPublisher(String publisher);
+	void pedirPublisher();
+	void crearJuego();
+	boolean altaJuego(Juego juego);
+	void imprimirInforme(ArrayList<Juego> lista);
 }
--- src/servicio/IGestor.java
+++ src/servicio/IGestor.java
@@ -4,6 +4,11 @@ import model.Juego;
 
 public interface IGestor {
 	void altaJuego();
-
 	void generarInforme();
+	void filtradoGenero();
+	void filtradoPorNintendo();
+	void filtradoPorPublihser();
+	void filtradoSXX();
+	void pedirPlataforma();
+	void filtradoAniosPares();
 }
