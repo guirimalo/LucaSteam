80f36222d22ee55e77d20e734d2af345968446b8
fatimapaternaroda <fatimapaternaroda@localhost>
2022-03-08 13:00:08 +0100

    test publisher y nintendo

--- src/test/TestPublisher.java
+++ src/test/TestPublisher.java
@@ -2,20 +2,48 @@ package test;
 
 import static org.junit.Assert.*;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
 import dato.Dato;
+import model.Genero;
 import model.Juego;
+import model.Plataforma;
 
 public class TestPublisher {
 
+	Dato d = new Dato();
+
+	private Juego juego1 = new Juego("Wii Sports", Plataforma.WII, 2006, Genero.SPORTS, "Nintendo");
+	ArrayList<Juego> lista = d.filtrarPorPublisher("Nintendo");
+
+
 	@Test
 	public void isNotNull() {
-		Dato d = new Dato();
-		assertNotNull(d.filtrarPorPublisher("Nintendo"));
+		assertNotNull(lista);
+		
 	}
-	
+
+
+	@Test
+	public void testPublisherFalse() {
+		juego1 = new Juego("Super Mario Bross", Plataforma.NES, 1985, Genero.PLATFORM, "Activision");
+		assertFalse(lista.contains(juego1));
+	}
+
+	@Test
+	public void testPublisherisNotEmpty() {
+		
+		assertTrue(lista.isEmpty());
+		
+	}
+
+	@Test
+	public void testPublisherisEmpty() {
+		ArrayList<Juego> lista2 = new ArrayList<Juego>();
+		assertTrue(lista2.isEmpty());
+	}
 }
